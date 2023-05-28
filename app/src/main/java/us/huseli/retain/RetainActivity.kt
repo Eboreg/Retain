025de09038fe156845b75b82281c623e8079178d
Robert Huselius <robert@huseli.us>
2023-05-28 22:26:32 +0200

    Splashscreen working on API < 31!

--- app/src/main/java/us/huseli/retain/RetainActivity.kt
+++ app/src/main/java/us/huseli/retain/RetainActivity.kt
@@ -3,6 +3,7 @@ package us.huseli.retain
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import us.huseli.retain.compose.App
 import javax.inject.Inject
@@ -13,6 +14,8 @@ class RetainActivity : ComponentActivity() {
     lateinit var logger: Logger
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContent {
             App(logger)
